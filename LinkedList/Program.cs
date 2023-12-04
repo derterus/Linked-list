5d88a7f4129532260a689e69f8111cea30d8ec3d
derterus <nikitarabcev69@gmail.com>
2023-12-04 11:01:50 +0800

    init

---
+++ LinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedList
+{
+    public class Item<T>
+    {
+        public Item(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public Item<T> Next { get; set; }
+    }
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+        }
+    }
+}
